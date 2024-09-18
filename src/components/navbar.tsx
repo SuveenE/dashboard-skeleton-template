fe9b875712243fc83dbb5d36a2c3a2781bb354cd
SuveenE <suveen.te1@gmail.com>
2024-09-17 23:20:03 -0700

    Update README.md

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,36 +1,35 @@
 import Link from "next/link";
 import React from "react";
-import { OrganizationSwitcher, SignOutButton, UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
     <div className="fixed inset-x-0 top-0 bg-gray-50  z-[10] h-fit  py-4 ">
       <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto">
         <div className="flex flex-row gap-3 justify-center">
-        <Link href={"/dashboard"} className="flex items-center gap-2">
-          <p className="px-2 py-1 text-2xl font-bold text-black">
-            Folo<span className="text-indigo-600">Up</span>{" "}
-            <span className="text-[8px]">Beta</span>
-          </p>
-        </Link>
-        <p className="my-auto text-xl">/</p>
-        <div className="my-auto">
-          <OrganizationSwitcher
-          afterCreateOrganizationUrl="/dashboard"
-          hidePersonal={true}
-          afterSelectOrganizationUrl="/dashboard"
-          afterLeaveOrganizationUrl="/dashboard"
-          appearance={{
-            variables: {
-              fontSize: "0.9rem"
-            }
-          }}
-        />
-        </div>
+          <Link href={"/dashboard"} className="flex items-center gap-2">
+            <p className="px-2 py-1 text-2xl font-bold text-black">
+              Folo<span className="text-indigo-600">Up</span>{" "}
+              <span className="text-[8px]">Beta</span>
+            </p>
+          </Link>
+          <p className="my-auto text-xl">/</p>
+          <div className="my-auto">
+            <OrganizationSwitcher
+              afterCreateOrganizationUrl="/dashboard"
+              hidePersonal={true}
+              afterSelectOrganizationUrl="/dashboard"
+              afterLeaveOrganizationUrl="/dashboard"
+              appearance={{
+                variables: {
+                  fontSize: "0.9rem",
+                },
+              }}
+            />
+          </div>
         </div>
         <div className="flex items-center">
-          
-        <UserButton signInUrl="/sign-in" /> 
+          <UserButton signInUrl="/sign-in" />
         </div>
       </div>
     </div>
